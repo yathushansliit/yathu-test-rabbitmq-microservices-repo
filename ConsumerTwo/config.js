ea70962c9070bd682a1c4255b4c0afa9a29f763f
Yathushan <rajendran.yathushan@bcstechnology.com.au>
2025-04-25 14:38:59 +0530

    change

--- ConsumerTwo/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    rabbitMQ : {
-        // url: "amqps://nwjdmsis:qvJpWbaFOOBeS3EOLcwgBoWVRFWbZbb2@shark.rmq.cloudamqp.com/nwjdmsis",
-        url:"amqps://b-9f5a8bef-c0ab-44c4-a9e7-3f3480e57428.mq.ap-northeast-3.on.aws:5671",
-        exchangeName : "awslogExchange"
-    }
-}

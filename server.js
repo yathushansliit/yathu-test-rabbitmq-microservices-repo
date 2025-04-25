f47a2bd0391e0c23f4a283717337c52bc39898ac
Yathushan <rajendran.yathushan@bcstechnology.com.au>
2025-04-25 17:01:11 +0530

    dfgvxdfv

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.post('/sendLog', async(req,res,next)=>{
 
 app.get('/', async(req,res,next)=>{
     
-    res.send("Helloooooooooooooooo");
+    res.send("Hellooohbuihv");
 })
 
 app.listen(3000, ()=>{
